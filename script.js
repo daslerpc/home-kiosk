be7a6a4a1b1a8e95c385a270be3223c1b76a54dc
daslerpc <philip.dasler@gmail.com>
2016-09-19 21:16:03 +0200

    Finished js migration

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+function setInspiration(element) {
+	var quotesToPull = 25;
+
+	var index = Math.floor((Math.random() * (quotesToPull - 1)));
+
+	$.getJSON("http://www.reddit.com/r/showerthoughts/hot/.json?count=" + quotesToPull, function(data) {
+		document.getElementById(element).innerHTML = data.data.children[index].data.title;	
+	});
+}
+
+function setImage(element) {
+
+	var imagesToPull = 25;
+	var index = Math.floor((Math.random() * (imagesToPull - 1)));
+
+	$.getJSON("http://www.reddit.com/r/earthporn/hot/.json?count="+imagesToPull, function(data) { 
+
+		var imageSource = data.data.children[index].data.url;
+		var imageLoaded = "unset";
+
+		imageLoaded = testImage(imageSource);
+		// document.getElementById("text-inspiration").innerHTML += imageLoaded;
+
+		// currently seems to have trouble with links that don't end in .jpg
+		document.getElementById(element).innerHTML += '<img src="' + imageSource + '">';
+	});
+}
+
 function testImage(url, timeout) {
 		var response = "unknown";
 	    timeout = timeout || 5000;
