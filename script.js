e173d786f63e91eea597747bf05ec2035be714fd
Phil <philip.dasler@gmail.com>
2016-09-20 23:07:23 +0200

    Image size "fix"
    Added CSS for #background_image which helps the problem of oversized
    images not fitting on the screen, but it’s not perfect.  Images can
    still be too tall and go beyond the fold.

--- script.js
+++ script.js
@@ -26,46 +26,14 @@ function setImage(element) {
 	$.getJSON("http://www.reddit.com/r/earthporn/hot/.json?count="+imagesToPull, function(data) { 
 
 		var imageSource = data.data.children[index].data.url;
-		var imageLoaded = "unset";
-
-		// This doesn't work currently
-		imageLoaded = testImage(imageSource, element);
 
 		// if it doesn't end in a picture extension, add .jpg
 		if ( imageSource.indexOf("imgur") != -1 && ! /\.(jpe?g|png|gif|bmp)$/i.test(imageSource) ) 
 			imageSource += ".jpg";
 
 		// currently seems to have trouble with links that don't end in .jpg
-		document.getElementById(element).innerHTML += '<img src="' + imageSource + '">';
+		document.getElementById(element).innerHTML += '<img id="background_image" src="' + imageSource + '">';
+
+		
 	});
 }
-
-function testImage(url, element, timeout) {
-		var response = "unknown";
-	    timeout = timeout || 5000;
-	    
-	    var timedOut = false, timer;
-	    var img = new Image();
-	    
-	    img.onerror = img.onabort = function() {
-	        if (!timedOut) {
-	            clearTimeout(timer);
-	            response = "error";
-	        }
-	    };
-	    
-	    img.onload = function() {
-	        if (!timedOut) {
-	            clearTimeout(timer);
-	            response = "success";
-	        }
-	    };
-	    
-	    img.src = url;
-	    timer = setTimeout(function() {
-	        timedOut = true;
-	        response = "timeout";
-	    }, timeout); 
-
-	    return response;
-	}
